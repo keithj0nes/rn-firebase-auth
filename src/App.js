4df7b7e63be5ffab6df08c5ddd4ff93a5840dec2
keith <keith@raincityambience.com>
2018-04-29 17:55:54 -0700

    Add file structure to reuse components with simple import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 class App extends React.Component {
   render(){
     return (
       <View>
+        <Header title='Authentication' />
         <Text> my app! </Text>
       </View>
     )
