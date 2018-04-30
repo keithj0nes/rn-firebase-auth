91ad0acafdcb48356ef617287d26e037c077bc33
keith <keith@raincityambience.com>
2018-04-29 17:24:19 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends React.Component {
+  render(){
+    return (
+      <View>
+        <Text> my app! </Text>
+      </View>
+    )
+  }
+}
+
+export default App;
